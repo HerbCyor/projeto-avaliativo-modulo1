eeff87a63a1c5501bf0ff39ef8ee18bbf29a95fe
Herbert <herbert.cardozo@gmail.com>
2024-05-04 13:02:11 -0300

    update .gitignore + freeze table names

--- src/config/database.config.js
+++ src/config/database.config.js
@@ -7,5 +7,8 @@ module.exports = {
     username: process.env.USERNAMEDB,
     password: process.env.PASSWORDDB,
     database: process.env.DATABASE,
-    port: process.env.PORT
+    port: process.env.PORT,
+    define: {
+        freezeTableName: true
+    }
 };
